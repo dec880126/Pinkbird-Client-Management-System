f28b092c7d80f772ea57e1e0e4e47328daa0386f
Cyuan <34447298+dec880126@users.noreply.github.com>
2021-08-23 01:10:28 +0800

    Add files via upload

---
+++ package/config.py
@@ -0,0 +1,61 @@
+import configparser
+import os
+
+# """
+# [Synology]
+# path = 
+# port = 
+# """
+
+def check_config_if_exist(path):
+    return bool(os.path.isfile(path))
+
+
+def load_config(path = "./config.ini"):
+    config = configparser.ConfigParser()    
+    config.read(path, encoding="utf-8")
+
+    return {
+        "host": config["Setting"]["host"],
+        "port": config["Setting"]["port"]
+    }
+
+
+def make_config(path: str, configMode: int):
+    """
+     - configMode == 1: make config of 'system setting'
+     - configMode == 2: make config of 'login log '
+     - configMode == 3: make config of 'operation log '
+    """
+    if configMode == 1:        
+        with open(path, "w", encoding="utf-8") as f:
+            system_config(f)
+    elif configMode == 2:
+        with open(path, "w", encoding="utf-8") as f:
+            login_log_config(f)
+    elif configMode == 3:
+        with open(path, "w", encoding="utf-8") as f:
+            operationLog_config(f)
+    else:
+        print("[!]config.py 中 make_config 函數之參數 configMode 設定錯誤")
+
+
+def write_config(path, content):
+    with open(path, "a", encoding="utf-8") as f:
+        f.write(content)
+
+
+def system_config(f):
+    f.write("[Setting]\n")
+    f.write("; host 為資料庫所在位置\n")
+    f.write("host = \n")
+    f.write("; port 為資料庫所對應之 IP 的連接埠\n")
+    f.write("port = ")
+
+def login_log_config(f):
+    f.write("[粉鳥旅行社會員資料庫管理系統 登入紀錄]\n")
+    f.write("       時間	             操作者		 狀態\n")
+
+def operationLog_config(f):
+    f.write("[粉鳥旅行社會員資料庫管理系統 操作紀錄]\n")
+    f.write("       時間	             操作者		 內容\n")
---
+++ package/year_cal.py
@@ -0,0 +1,59 @@
+import datetime
+
+def is_leap(year):
+    if year % 400 == 0 or year % 40 == 0 or year % 4 == 0:
+        return True
+    else:
+        return False
+
+month_days = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+def minus_result(first_year, second_year):
+    y = first_year.year - second_year.year
+    m = first_year.month - second_year.month
+    d = first_year.day - second_year.day
+    if d < 0:
+        if second_year.month == 2:
+            if is_leap(second_year.year):
+                month_days[2] = 29
+        d += month_days[second_year.month]
+        m -= 1
+    if m < 0:
+        m += 12
+        y -= 1
+    if y == 0:
+        if m == 0:
+            return round(d/365, 2)
+        else:
+            return round((m*30+d)/365, 2)
+    else:
+        return y
+
+# dotdot = datetime.date(
+#     year=int(input('请输入你的出生年份：')),
+#     month=int(input('请输入你的出生月份：')),
+#     day=int(input('请输入你的出生日期：'))
+# )
+
+# t = datetime.date(
+#     year=int(input('請輸入出團年份：')),
+#     month=int(input('請輸入出團月份：')),
+#     day=int(input('請輸入出團日期：'))
+# )
+
+# birthList = ['64.02.28', '65.12.29', '48.11.17', '71.05.28']
+
+
+
+def get_years_old(birth, day):
+    
+
+    birthSpilt = birth.split('.')
+    birth = datetime.date(
+        year=int(birthSpilt[0])+1911,
+        month=int(birthSpilt[1]),
+        day=int(birthSpilt[2])
+    )
+    return minus_result(day, birth)
+
+# for birth in birthList:
+#     print(get_years_old(birth))
