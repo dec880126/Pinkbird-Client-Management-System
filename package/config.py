3e1145ea9ededfc228fd7ca9429b6396e27c0437
Cyuan <dec880126@gmail.com>
2021-11-15 10:11:53 +0800

    Revert "支援v7的config讀取"
    This reverts commit ef4c033fea7984f86b328569c5ef8a67be85e76a.

--- package/config.py
+++ package/config.py
@@ -17,10 +17,7 @@ def load_config(path = "./config.ini"):
 
     return {
         "host": config["Setting"]["host"],
-        "port": config["Setting"]["port"],
-        "log-user": config["Log"]["user"],
-        "log-password": config["Log"]["password"],
-        "log-database": config["Log"]["database"]
+        "port": config["Setting"]["port"]
     }
 
 
